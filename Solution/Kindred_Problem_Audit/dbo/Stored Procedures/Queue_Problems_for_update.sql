958157fb8a8a5f8aefe30939c7b943c409e8d10b
scott-stover-centric <scott.stover@centricconsulting.com>
2018-10-17 21:26:10 -0400

    Problem Monitoring Updates
    Updates to the Kindred Problem Audit process

---
+++ Solution/Kindred_Problem_Audit/dbo/Stored Procedures/Queue_Problems_for_update.sql
@@ -0,0 +1,54 @@
+﻿
+CREATE PROCEDURE [dbo].[Queue_Problems_for_update]
+AS
+
+/*
+
+This procedure de-duplicates the problem_monitoring table and compares the latest the latest entries
+from Kindred_Audit_List and compares to the Problem_Monitoring table.  It will flag any problems ready
+for update in Problem_Monitoring
+
+*/
+
+-- Remove Duplicates
+DELETE FROM Problem_Monitoring
+WHERE ProblemID IN
+(
+	SELECT ProblemID FROM
+	(
+	SELECT MRN, AssessmentDate, sfc_Agency__c, MIN(ProblemID) FirstID, COUNT(*) RecordCount
+	FROM Problem_Monitoring
+	GROUP BY MRN, AssessmentDate, sfc_Agency__c
+	) DupAssessments
+	INNER JOIN
+	(
+		SELECT ProblemID, MRN, AssessmentDate, sfc_Agency__c
+		FROM Problem_Monitoring
+		WHERE ProblemID NOT IN
+		(
+			SELECT FirstID FROM
+			(
+				SELECT MRN, AssessmentDate, sfc_Agency__c, MIN(ProblemID) FirstID, COUNT(*) RecordCount
+				FROM Problem_Monitoring
+				GROUP BY MRN, AssessmentDate, sfc_Agency__c
+			) DA
+			WHERE FirstID IS NOT NULL
+		)
+	) OtherAssessments
+	ON DupAssessments.MRN = OtherAssessments.MRN
+	AND DupAssessments.AssessmentDate = OtherAssessments.AssessmentDate
+	AND DupAssessments.sfc_Agency__c = OtherAssessments.sfc_Agency__c
+	WHERE DupAssessments.RecordCount > 1
+)
+
+-- Update Queued Status
+UPDATE Problem_Monitoring
+SET update_ready_ind = 1, update_status = 'Queued', modify_timestamp = GETDATE(), problem_resolved_timestamp = GETDATE()
+WHERE update_ready_ind = 0
+AND problem_resolved_timestamp IS NULL
+AND update_timestamp IS NULL
+AND MRN NOT IN
+(
+	SELECT MRN FROM Kindred_Audit_List
+	WHERE MRN IS NOT NULL
+)
---
+++ Solution/Kindred_Problem_Audit/dbo/Tables/Problem_Monitoring.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Problem_Monitoring] (
+    [ProblemID]                  INT           IDENTITY (1, 1) NOT NULL,
+    [MRN]                        VARCHAR (200) NULL,
+    [AssessmentDate]             DATETIME      NULL,
+    [sfc_Agency__c]              NVARCHAR (18) NULL,
+    [Agency]                     VARCHAR (MAX) NULL,
+    [sfc_Status__c]              VARCHAR (200) NULL,
+    [create_timestamp]           DATETIME      CONSTRAINT [DF_Problem_Monitoring_create_timestamp] DEFAULT (getdate()) NOT NULL,
+    [modify_timestamp]           DATETIME      CONSTRAINT [DF_Problem_Monitoring_modify_timestamp] DEFAULT (getdate()) NOT NULL,
+    [problem_resolved_timestamp] DATETIME      NULL,
+    [update_ready_ind]           BIT           CONSTRAINT [DF_Problem_Monitoring_update_hhcp_ready_ind] DEFAULT ((0)) NOT NULL,
+    [update_status]              VARCHAR (25)  NULL,
+    [update_status_details]      VARCHAR (MAX) NULL,
+    [update_timestamp]           DATETIME      NULL,
+    CONSTRAINT [PK_Problem_Monitoring] PRIMARY KEY CLUSTERED ([ProblemID] ASC)
+);
+
